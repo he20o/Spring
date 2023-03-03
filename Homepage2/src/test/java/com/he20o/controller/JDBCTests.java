3496bc9b1717be43e1227bb0e975ff4c81d2523b
GDJ 59 <GDJ 59@GDJ59>
2023-03-03 15:58:51 +0900

    spring 홈페이지

---
+++ Homepage2/src/test/java/com/he20o/controller/JDBCTests.java
@@ -0,0 +1,34 @@
+package com.he20o.controller;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.junit.Test;
+
+import lombok.extern.log4j.Log4j;
+import static org.junit.Assert.fail;
+
+@Log4j
+public class JDBCTests {
+
+	static {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testConnection() {
+		try(Connection con =
+				DriverManager.getConnection(
+							"jdbc:mysql://localhost:3306/paris",
+							"root",
+							"0000")){
+			log.info(con);;
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+}
